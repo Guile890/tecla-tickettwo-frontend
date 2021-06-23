3b05c600624b93e4e085fd1f264f53ae6160f0c7
Guilebaldo Munguia <guilebaldo.munguia@gnp.com.mx>
2021-06-23 11:33:19 -0500

    add token

--- tecla-connect/src/app/services/servicemain.service.ts
+++ tecla-connect/src/app/services/servicemain.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
@@ -17,9 +17,11 @@ export class ServicemainService {
     return this.registrarseService(data).toPromise();
   }
   public registrarseService(data): Observable<any> {
+    const tokenBack = "Bearer" + sessionStorage.getItem('token')
+    const httpParams = new HttpParams().set("access_token", tokenBack)
     const body = { data }
     const url = this.servidor + 'usuario';
-    return this.httpClient.post<[]>(url, body);
+    return this.httpClient.post<[]>(url, body, {params: httpParams});
   }
   public login(data): any{
     return this.loginService(data).toPromise();
